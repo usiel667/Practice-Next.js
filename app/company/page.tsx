ece225241a276a728bdb0e362ee024aa337500ba
usiel667 <usiel667@users.noreply.github.com>
2025-09-15 23:16:10 -0600

    add company page

---
+++ app/company/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+//import styles from "./page.module.css";
+import image from "next/image";
+
+export default function Company() {
+  return <p>Company Page Under Construction - Coming Soon!</p>;
+}
